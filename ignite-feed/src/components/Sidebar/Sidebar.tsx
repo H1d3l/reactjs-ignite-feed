873bf9346537ca12175c0b72b904103c990df034
H1d3l <hidels.rios@gmail.com>
2023-11-16 10:01:14 -0300

    projeto finalizado

--- ignite-feed/src/components/Sidebar/Sidebar.tsx
+++ ignite-feed/src/components/Sidebar/Sidebar.tsx
@@ -1,21 +1,25 @@
+import { Avatar } from "../Avatar/Avatar";
 import styles from "./Sidebar.module.css";
-import { PencilSimpleLine  } from "@phosphor-icons/react";
+import { PencilSimpleLine } from "@phosphor-icons/react";
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img className={styles.cover} src="https://images.unsplash.com/photo-1589466725882-f47191476e8c?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1589466725882-f47191476e8c?q=60&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
 
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/H1d3l.png"/>
-        <strong>Hidelbrando Rios</strong>
+        <Avatar src="https://github.com/diego3g.png" />{" "}
+        <strong>Diego Fernandes</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        
         <a href="#">
-            <PencilSimpleLine size={20}/>
-            Editar seu perfil</a>
+          <PencilSimpleLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
